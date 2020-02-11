0cc53db92482c19a60640dd83db8dbc12779cc67
ryan.herrmann <ryan.herrmann@mu-c1-17.mullen.pvt.k12.co.us>
2020-02-11 11:43:09 -0700

    Starting to read and place teh properties in

---
+++ Monopoly Master/src/ReadAndPlaceProperties.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadAndPlaceProperties
+	{
+		public static void readColorProperties() throws FileNotFoundException
+		{
+			Scanner file = new Scanner(new File("listOfProperties.txt"));
+			
+		}
+	}
