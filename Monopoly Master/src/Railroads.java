f6cb522d99ec4ad6af757bbd5b81982bfb529f03
tanner.amman <tanner.amman@MU-C1-09.mullen.pvt.k12.co.us>
2020-02-11 11:42:54 -0700

    finished buyable properties

--- Monopoly Master/src/Railroads.java
+++ Monopoly Master/src/Railroads.java
@@ -1,12 +1,22 @@
 
-public class Railroads extends Board
+public class Railroads extends BuyableProperties
 	{
-		protected int price;
+		protected int numberOwned;
 		
-	public Railroads(String n)
+	public Railroads(String n, int p, boolean b, int cWLO, int nO)
 		{
-			super(n);
-			
+			super(n, p, b, cWLO);
+			numberOwned = nO;
+		}
+
+	public int getNumberOwned()
+		{
+			return numberOwned;
+		}
+
+	public void setNumberOwned(int numberOwned)
+		{
+			this.numberOwned = numberOwned;
 		}
 
 	}
