674965f45284434bc545eccd790534bbc88b2bf6
tanner.amman <tanner.amman@MU-C1-09.mullen.pvt.k12.co.us>
2020-02-13 11:36:16 -0700

    did more to dice class

--- Monopoly Master/src/Dice.java
+++ Monopoly Master/src/Dice.java
@@ -1,12 +1,15 @@
 
 public class Dice
 	{
+		static int diceTotal;
+		static boolean isDoubles;
+
 		public static int rollDice()
 		{
 			boolean isDoubles = false;
 			int dice1 = (int) (Math.random() * 6) + 1;
 			int dice2 = (int) (Math.random() * 6) + 1;
-			int diceTotal = dice1 + dice2;
+			diceTotal = dice1 + dice2;
 			if (dice1 == dice2)
 				{
 					isDoubles = true;
