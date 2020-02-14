d3138564626b172461bd594ea58c38e437f1fb47
danielle.failor <danielle.failor@MU-C1-02.mullen.pvt.k12.co.us>
2020-02-14 10:09:41 -0700

    I just do.

--- Monopoly Master/src/Playing.java
+++ Monopoly Master/src/Playing.java
@@ -1,5 +1,12 @@
 
 public class Playing
 	{
-
+		static boolean playing;
+		public static void play()
+		{
+			while(playing)
+				{
+				//	System.out.println("You are at  " +Board.board.);
+				}
+		}
 	}
