071a995b96b02c9db62b50bfe67ebfce25152df0
danielle.failor <danielle.failor@MU-C1-02.mullen.pvt.k12.co.us>
2020-02-20 11:04:39 -0700

    commit again

--- Monopoly Master/src/Playing.java
+++ Monopoly Master/src/Playing.java
@@ -1,53 +1,4 @@
-<<<<<<< HEAD
 
-public class Playing
-	{
-
-		static int player =1;
-
-		static boolean playing;
-		public static void play()
-		{
-			while(playing)
-				{
-					while(playing)
-						{
-							player--;
-							System.out.println("Hello "+MonopolyRunner.players.get(player).getName()+" are at  " + MonopolyRunner.board.get(MonopolyRunner.players.get(player).getPlaceOnBoard()) +".");
-							Dice.rollDice();
-
-							MonopolyRunner.players.get(player).setPlaceOnBoard(MonopolyRunner.players.get(player).getPlaceOnBoard() + Dice.diceTotal);;
-							System.out.println(MonopolyRunner.players.get(player).getName()+ ", you rolled a total of " +Dice.diceTotal +". You landed on " + MonopolyRunner.board.get(MonopolyRunner.players.get(player).getPlaceOnBoard())+".");
-							OptionsWhenYouLand.landOnPlace();
-							player++;
-							System.out.println("Hello "+MonopolyRunner.players.get(player).getName()+" are at  " + MonopolyRunner.board.get(MonopolyRunner.players.get(player).getPlaceOnBoard()) +".");
-
-							Dice.rollDice();
-							MonopolyRunner.players.get(player).setPlaceOnBoard(MonopolyRunner.players.get(player).getPlaceOnBoard() + Dice.diceTotal);;
-							System.out.println(MonopolyRunner.players.get(player).getName()+ ", you rolled a total of " +Dice.diceTotal +". You landed on " + MonopolyRunner.board.get(MonopolyRunner.players.get(player).getPlaceOnBoard())+".");
-						}
-					
-				}
-		}
-		
-//		public static void landOnProperty()
-//		{
-//			
-//		}
-//		public static void landOnTax()
-//		{
-//				
-//		}
-//		public static void communityCard()
-//		{
-//			
-//		}
-//		public static void chanceCard()
-//		{
-//				
-//		}
-	}
-=======
 
 public class Playing
 	{
@@ -93,4 +44,3 @@ public class Playing
 //				
 //		}
 	}
->>>>>>> upstream/master
